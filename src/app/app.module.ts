2bc1acecccf81f5be7b7f57ca80683b2b4f1c8fe
vijeeshvariyath <vijeeshvariyath0@gmail.com>
2022-09-30 16:03:12 +0530

    Login page and Dashboard Accessment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginpageComponent } from './loginpage/loginpage.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginpageComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
